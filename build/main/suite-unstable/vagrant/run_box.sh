18c44fe11fb9000a8485fab5cf54a2a4303620f5
Michael Weisman <mweisman@gmail.com>
2012-06-04 10:49:54 -0700

    Moved boxes over to puppet and run_box.sh rather than canned shell scripts

---
+++ build/main/suite-unstable/vagrant/run_box.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+set -e
+
+if [[ -z $1 ]]; then
+    echo "ERROR: Missing box name"
+    echo "usage: run_box.sh <box> <repo>"
+    exit 1
+else
+    BOX_NAME=$1
+fi
+
+if [[ -z $2 ]]; then
+    echo "ERROR: Missing repo name"
+    echo "usage:
+    echo "run_box.sh <box> <repo>"
+    exit 1
+else
+    REPO=$2
+fi
+
+apply_file="class {'opengeosuite':\
+  repo  => '${REPO}'\
+}"
+
+cd $BOX_NAME
+echo $apply_file > manifests/apply.pp
+echo "Building ${REPO} on ${BOX_NAME}"
+vagrant up
